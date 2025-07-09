36704ee32b69519be711ff0a1b2fecff0db88ac1
Luan H. Nebbering <78267195+nebbering@users.noreply.github.com>
2025-07-09 16:02:22 -0300

    update

--- cypress/e2e/test-orangehrm.cy.js
+++ cypress/e2e/test-orangehrm.cy.js
@@ -2,6 +2,17 @@ import userData from '../fixtures/dataUser.json'
 
 describe('Testes de Login e Preenchimento de Campos', () => {
 
-    beforeEach
+const selectorsList = {
+    Title: 'OrangeHRM',
+    loginInput: "[name='username']"
+}
+
+    beforeEach(() => {
+        cy.visit('/auth/login')
+    })
+
+    it('login-bem-sucedido', () => {
+        cy.get(selectorsList.loginInput).type(userData.UserValid.userName)
+    })
 
 })
