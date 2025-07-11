41fa3377842a7d688f56e63a047824b3153071d3
Luan H. Nebbering <78267195+nebbering@users.noreply.github.com>
2025-07-11 16:57:29 -0300

    update

--- cypress/e2e/test-orangehrm.cy.js
+++ cypress/e2e/test-orangehrm.cy.js
@@ -7,7 +7,19 @@ const selectorsList = {
     Title: 'OrangeHRM',
     elementLogin: ':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input',
     loginInput: "[name='username']",
-    PasswordInput: "[name='password']",
+    passwordInput: "[name='password']",
+    firstNameInput: "[name='firstName']",
+    middleNameInput: "[name='middleName']",
+    lastNameInput: "[name='lastName']",
+    employeeIdInput: ':nth-child(1) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input',
+    otherIdInput: ':nth-child(3) > :nth-child(1) > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input',
+    driverLicenseInput: ':nth-child(2) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input',
+    licenseExpiryInput: ':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-input',
+    nationalityInput: '',
+    maritionalStatusButton: ':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text',
+    maritialStatusInput: '',
+
+
 
     //Buttons
     btnLogin: ".oxd-button",
@@ -25,7 +37,7 @@ const selectorsList = {
 
         //Entradas
         cy.get(selectorsList.loginInput).type(userData.UserValid.userName),
-        cy.get(selectorsList.PasswordInput).type(userData.UserValid.userPassword),
+        cy.get(selectorsList.passwordInput).type(userData.UserValid.userPassword),
         cy.get(selectorsList.btnLogin).click(),
         cy.url().should('include', 'dashboard')
     })
@@ -38,12 +50,21 @@ const selectorsList = {
 
         //Entradas
         cy.get(selectorsList.loginInput).type(userData.UserValid.userName),
-        cy.get(selectorsList.PasswordInput).type(userData.UserValid.userPassword),
+        cy.get(selectorsList.passwordInput).type(userData.UserValid.userPassword),
         cy.get(selectorsList.btnLogin).click(),
         cy.url().should('include', 'dashboard')
 
         cy.get(selectorsList.btnMyInfo).click(),
         cy.url().should('include', 'viewPersonalDetails')
+
+        cy.get(selectorsList.firstNameInput).clear().type(userData.myInfo.firstName),
+        cy.get(selectorsList.middleNameInput).clear().type(userData.myInfo.middleName),
+        cy.get(selectorsList.lastNameInput).clear().type(userData.myInfo.lastName),
+        cy.get(selectorsList.employeeIdInput).clear().type(userData.myInfo.employeeId),
+        cy.get(selectorsList.otherIdInput).clear().type(userData.myInfo.otherId),
+        cy.get(selectorsList.driverLicenseInput).clear().type(userData.myInfo.driverLicense),
+        cy.get(selectorsList.licenseExpiryInput).clear().type(userData.myInfo.liceseExpiry),
+        cy.get('.dropdown-menu').click()
     })
 
 })
