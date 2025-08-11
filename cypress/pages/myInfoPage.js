1008cce8ee818e490af48cc07b1e7caf77db0e7c
Luan H. Nebbering <78267195+nebbering@users.noreply.github.com>
2025-08-11 14:46:03 -0300

    update

--- cypress/pages/myInfoPage.js
+++ cypress/pages/myInfoPage.js
@@ -10,10 +10,16 @@ class myInfoPage {
             firstNameInput: "[name='firstName']",
             middleNameInput: "[name='middleName']",
             lastNameInput: "[name='lastName']",
-            employeeIdInput: ":nth-child(1) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input",
+            employeeIdInput: ":nth-child(3) > :nth-child(1) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input",
             otherIdInput: ":nth-child(3) > :nth-child(1) > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input",
             driverLicenseNumberInput: ":nth-child(2) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input",
             licenseExpiryDateInput: ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-input",
+
+            nationalityBox: ":nth-child(5) > :nth-child(1) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text--after > .oxd-icon",
+            nationalityInput: ".oxd-select-dropdown > :nth-child(20)", //Brasil
+            maritalStatusInput: ":nth-child(4) > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-select-text-input",
+            dateOfBirthInput: ":nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-input",
+            genderInput: ":nth-child(5) > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-select-text-input",
 
         }
         return selectors
@@ -24,20 +30,24 @@ class myInfoPage {
         cy.get(this.selectorsList().btnMyInfo).click()
         cy.url().should('include', 'viewPersonalDetails')
 
-        //dados Pessoais
+        //Dados Pessoais
         cy.get(this.selectorsList().firstNameInput).clear().type('Okabe'),
         cy.get(this.selectorsList().middleNameInput).clear().type('Kyouma'),
         cy.get(this.selectorsList().lastNameInput).clear().type('Rintarou'),
          
-        // Sessão Employee
-        cy.get(this.selectorsList().employeeIdInput).clear().type('979797'),
-        cy.get(this.selectorsList().otherIdInput).clear().type('979797979'),
-        cy.get(this.selectorsList().driverLicenseNumberInput).clear().type('9797979797'),
+        cy.get(this.selectorsList().employeeIdInput).clear().type('111111'),
+        cy.get(this.selectorsList().otherIdInput).clear().type('222222222'),
+        cy.get(this.selectorsList().driverLicenseNumberInput).clear().type('3333333333'),
         cy.get(this.selectorsList().licenseExpiryDateInput).clear().type('2025-12-31'),
 
+        cy.get(this.selectorsList().nationalityBox).click({force: true}),
+        cy.get(this.selectorsList().nationalityInput).click({force: true}),
+        //cy.get(this.selectorsList().dateOfBirthInput).clear().type('20212-04-07'),
+
+
         // Clica no botão de salvar e verifica se a mensagem de sucesso é exibida
-        cy.get(this.selectorsList().btnSavePersonalDetails).click(),
-        cy.get(this.selectorsList().msgSaveSuccess).should('be.visible').and('contain', 'Successfully Updated')
+        //cy.get(this.selectorsList().btnSavePersonalDetails).click(),
+        //cy.get(this.selectorsList().msgSaveSuccess).should('be.visible').and('contain', 'Successfully Updated')
 
     }
 
